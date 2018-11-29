823841e843b919e2316210fd0f60bbd6060d16aa
Sun <kimson305@gmail.com>
2018-11-29 14:16:26 +0900

    초기 커밋.

---
+++ test_1/test_1View.cpp
@@ -0,0 +1,81 @@
+﻿
+// test_1View.cpp: Ctest1View 클래스의 구현
+//
+
+#include "stdafx.h"
+// SHARED_HANDLERS는 미리 보기, 축소판 그림 및 검색 필터 처리기를 구현하는 ATL 프로젝트에서 정의할 수 있으며
+// 해당 프로젝트와 문서 코드를 공유하도록 해 줍니다.
+#ifndef SHARED_HANDLERS
+#include "test_1.h"
+#endif
+
+#include "test_1Doc.h"
+#include "test_1View.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// Ctest1View
+
+IMPLEMENT_DYNCREATE(Ctest1View, CView)
+
+BEGIN_MESSAGE_MAP(Ctest1View, CView)
+END_MESSAGE_MAP()
+
+// Ctest1View 생성/소멸
+
+Ctest1View::Ctest1View() noexcept
+{
+	// TODO: 여기에 생성 코드를 추가합니다.
+
+}
+
+Ctest1View::~Ctest1View()
+{
+}
+
+BOOL Ctest1View::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: CREATESTRUCT cs를 수정하여 여기에서
+	//  Window 클래스 또는 스타일을 수정합니다.
+
+	return CView::PreCreateWindow(cs);
+}
+
+// Ctest1View 그리기
+
+void Ctest1View::OnDraw(CDC* /*pDC*/)
+{
+	Ctest1Doc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 여기에 원시 데이터에 대한 그리기 코드를 추가합니다.
+}
+
+
+// Ctest1View 진단
+
+#ifdef _DEBUG
+void Ctest1View::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void Ctest1View::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+Ctest1Doc* Ctest1View::GetDocument() const // 디버그되지 않은 버전은 인라인으로 지정됩니다.
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(Ctest1Doc)));
+	return (Ctest1Doc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// Ctest1View 메시지 처리기
